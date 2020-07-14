22052935859800e6a5acfec8012b06829db0616c
Tomasz Dmowski <dmowski.tomek@gmail.com>
2020-07-14 23:59:26 +0200

    Get elements to js file

---
+++ app.js
@@ -0,0 +1,4 @@
+const pokeList = document.querySelector('.pokemon-list');
+const filterType = document.getElementById('pokemon-filter-type');
+const filterEdition = document.getElementById('pokemon-filter-edition');
+const pagination = document.getElementById('pagination');
